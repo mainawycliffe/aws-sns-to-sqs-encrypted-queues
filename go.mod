1c153d0fbe3e1007de4d7e000985de6b7ff31b00
Maina Wycliffe <wmmaina7@gmail.com>
2020-03-24 00:21:42 +0300

    Implement SQS Messages Processing

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mainawycliffe/aws-sns-to-sqs-encrypted-queue
 
 go 1.14
 
-require github.com/aws/aws-lambda-go v1.15.0
+require (
+	github.com/aws/aws-lambda-go v1.15.0
+	github.com/davecgh/go-spew v1.1.1
+)
--- src/sendWelcomeEmail.go
+++ src/sendWelcomeEmail.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
-	fmt.Printf("Hello World")
+type User struct {
+	Email    string `json:"email"`
+	FullName string `json:"name"`
+	Phone    string `json:"phone"`
+}
+
+func sendWelcomeEmailHandler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
-		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+		// this message is an SNS Record(SNSEntity), to get to the message, we need to
+		// decode it and get the body
+		snsMessage := events.SNSEntity{}
+		err := json.Unmarshal([]byte(message.Body), &snsMessage)
+		if err != nil {
+			panic(err)
+		}
+		publishedMessage := User{}
+		err = json.Unmarshal([]byte(snsMessage.Message), &publishedMessage)
+		if err != nil {
+			panic(err)
+		}
+		// send welcome email here
+		fmt.Printf("Welcome email sent to: \t %s<%s> \n", publishedMessage.FullName, publishedMessage.Email)
 	}
 	return nil
 }
 
 func main() {
-	lambda.Start(handler)
+	lambda.Start(sendWelcomeEmailHandler)
 }
